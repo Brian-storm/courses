2c9af8c46f379682f9cedbe1f1d2d29bd11e01e4
Brian-storm <computer6ai@gmail.com>
2025-09-28 02:05:45 +0800

    CSCI2100 asg1

---
+++ CSCI2100/assignment_1/q3/queue.h
@@ -0,0 +1,17 @@
+/*
+* File: queue.h
+* Bold Lines are modified or newly added.
+*/
+
+#include <stdlib.h>
+
+typedef struct queueCDT *queueADT;
+typedef struct customerCDT *customerADT;
+typedef customerADT queueElementT;
+
+queueADT EmptyQueue(void);
+void Enqueue(queueADT, queueElementT);
+queueElementT Dequeue(queueADT);
+queueElementT QueueFrontPeek(queueADT);
+int QueueLength(queueADT);
+int QueueIsEmpty(queueADT);
